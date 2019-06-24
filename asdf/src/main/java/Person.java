6e18a5d50ea453fb249314d1b926e96c04f42e9f
xietingkai <xietingkai001@163.com>
2019-06-24 19:24:37 +0800

    person

--- asdf/src/main/java/Person.java
+++ asdf/src/main/java/Person.java
@@ -1,7 +1,44 @@
+
 /**
  * @author xtk
  */
 public class Person {
-  
-  String name = ""
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+class Cat{
+
+    String name;
+    int age;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
